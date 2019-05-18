ce5d2856ddc6fafb4e3d82c91cc49f0a6457ab07
waghtot <waghtot@gmail.com>
2019-05-18 21:02:51 +0100

    changes

---
+++ app/tasks/task_0001_nbp_fxrate_table_a.php
@@ -0,0 +1,3 @@
+<?php
+echo "nbp table_a";
+?>
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+
 /*
  * command line and online service
  */
 echo "welcome in nbp scheduler step one";
+//check if something came from cli
+//if(isset($argv[1]) && !empty($argv[1]))
+//{
+	$json = json_decode(file_get_contents('/home/pi/nbp/tab_a/raw/20190214.json', FILE_USE_INCLUDE_PATH));
+	echo "<pre>".print_r($json, 1)."</pre>";
+  
+//}
 ?>
